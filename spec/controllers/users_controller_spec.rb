3011e145c81d5970250a3a3b808b797d109b11d9
Anniebundy <ana.afonso.01@gmail.com>
2016-06-27 20:57:26 +0200

    Replaced fixtures with factories

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,40 +1,41 @@
 require 'rails_helper'
 
 describe UsersController, :type => :controller do
-  
-  before do
-  	@user1 = FactoryGirl.create(:user)
-  	@user2 = FactoryGirl.create(:user)
-    #@user1 = User.create!(email: "potato2@example.com", password: "123456")
-    #@user2 = User.create!(email: "kartofel1@example.com", password: "1234567")
-  end
+ 
+ before do
+     @user1 = FactoryGirl.create(:user)
+     @user2 = FactoryGirl.create(:user)
+   #@user1 = User.create!(email: "potato2@example.com", password: "123456")
+   #@user2 = User.create!(email: "kartofel1@example.com", password: "1234567")
+ end
 
-describe "GET #show" do
-  context "User is logged in" do  
-    before do
-      sign_in @user1
-    end
-end
-    it "loads correct user details"   do
-      get :show, id: @user1.id
-      expect(response).to have_http_status(200)
-      expect(assigns(:user)).to eq @user1
-    end
+ describe "GET #show" do
+   context "User is logged in" do  
+     before do
+       sign_in @user1
+     end
+
+     it "loads correct user details"   do
+       get :show, id: @user1.id
+       expect(response).to have_http_status(200)
+       expect(assigns(:user)).to eq @user1
+     end
+   end
  
 
-  context "No user is logged in" do
-    it "redirects to login" do
-      get :show, id: @user1.id
-      expect(response).to redirect_to(new_user_session_path)
-    end
-  end
-    
-    context "Attempt to access show page of user2" do
-      it "redirects to login" do
-        get :show, id: @user2.id 
-        expect(assigns(:user)).not_to eq @user2
-        expect(response).to redirect_to(root_path)
-      end
-    end
-  end
+   context "No user is logged in" do
+     it "redirects to login" do
+       get :show, id: @user1.id
+       expect(response).to redirect_to(new_user_session_path)
+     end
+   end
+     
+   context "Attempt to access show page of user2" do
+     it "redirects to login" do
+       get :show, id: @user2.id 
+       expect(assigns(:user)).not_to eq @user2
+       expect(response).to redirect_to(new_user_session_path)
+     end
+   end
+ end
 end
